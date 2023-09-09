5be957d948127b2799a0fe2d1c1e2ce001351dce
aariv <aariv.tmk@gmail.com>
2023-09-09 20:32:35 +0530

    added js file and verified email field

---
+++ script.js
@@ -0,0 +1,16 @@
+var email = document.getElementById("emailField");
+var submit_btn = document.getElementById("submitBtn");
+
+// verify email filed is empty or not
+
+function verifyEmail() {
+  let emailValue = email.value;
+  if (emailValue === "") {
+    alert("hey, please enter email");
+  } else {
+    alert("thanks for the registration");
+    console.log("email value is", emailValue);
+  }
+}
+
+submit_btn.addEventListener("click", verifyEmail);
